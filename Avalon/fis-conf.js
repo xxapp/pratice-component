f2b9d11281dc78f93b972fb9fc2057389a660672
mengxiangxiang <mengxiangxiang@cdavatar.com>
2016-09-18 17:03:53 +0800

    引入bootstarp模板，实现分页组件

--- Avalon/fis-conf.js
+++ Avalon/fis-conf.js
@@ -1,21 +1,105 @@
+// npm install [-g] fis3-hook-commonjs
 fis.hook('commonjs', {
-    extList: ['.js']
+    paths: {
+        avalon: '/vendor/avalon/avalon.js',
+        mmState: '/vendor/mmState/mmState.js',
+        jquery: '/vendor/jquery/jquery.js'
+    }
 });
 
-fis.unhook('components'); 
+fis.set('baseurl', '/test');
+fis.set('local_server', 'http://127.0.0.1:7070/cargotmall/');
+fis.set('remote_server', 'http://schy.youarebug.com/cargotmall/');
+fis.set('local_api', 'http://127.0.0.1:7070/cargotmall/api');
+fis.set('remote_api', 'http://schy.youarebug.com/cargotmall/api');
+fis.set('local_springapi', 'http://127.0.0.1:7070/cargotmall/home/');
+fis.set('remote_springapi', 'http://schy.youarebug.com/cargotmall/home/');
+
+
+fis.unhook('components');
 fis.hook('node_modules', {
-  shimBuffer: false
+    ignoreDevDependencies: true,
+    shimBuffer: false,
+    shimProcess: false
 });
 
-// 设置成是模块化 js, 编译后会被 define 包裹。
-fis.match('/{node_modules,components}/**.js', {
-  isMod: true
+fis.match('/{node_modules,components,services}/**/*.js', {
+    isMod: true, // 设置 comp 下都是一些组件，组件建议都是匿名方式 define
+    release: '/static/$0'
 });
+fis.match('/{node_modules,components}/**/*.html', {
+    release: false
+});
+fis.match('/services/*.js', {
+    isMod: true,
+    release: '/static/$0'
+});
+fis.match('/services/*.js', {
+    postprocessor: function (content, file, settings) {
+        return content
+            .replace('__API_URL__', fis.get('local_api'))
+            .replace('__SPRING_API_URL__', fis.get('local_springapi'));
+    }
+});
+fis.match('/static/ueditor/ueditor.config.js', {
+    postprocessor: function (content, file, settings) {
+        return content
+            .replace('__SERVER_URL__', fis.get('local_server'));
+    }
+});
+fis.match('/vendor/**/*.js', {
+    isMod: true,
+    release: '/static/$0'
+});
+fis.match('/{README.md,Readme.txt,cmd.cmd,package.json}', {
+    release: false
+})
 
 fis.match('::package', {
-  // 本项目为纯前段项目，所以用 loader 编译期加载，
-  // 如果用后端运行时框架，请不要使用。
-  postpackager: fis.plugin('loader', {
-    useInlineMap: true
-  })
-});
+    // npm install [-g] fis3-postpackager-loader
+    // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
+    postpackager: fis.plugin('loader', {
+        resourceType: 'commonJs',
+        useInlineMap: true
+    })
+})
+
+// fis3 release prod 产品发布，进行合并
+fis.media('prod')
+    // .match('*.js', {
+    //     packTo: '/static/aio.js'
+    // })
+    // .match('/static/mod.js', {
+    //     packOrder: -100
+    // });
+    .match('/static/**', {
+        url: '${baseurl}/$0'
+    })
+    .match('/{node_modules,components,services}/**/*.js', {
+        url: '${baseurl}/static/$0'
+    })
+    .match('/services/*.js', {
+        url: '${baseurl}/static/$0'
+    })
+    .match('/services/*.js', {
+        postprocessor: function (content, file, settings) {
+            return content
+                .replace('__API_URL__', fis.get('remote_api'))
+                .replace('__SPRING_API_URL__', fis.get('remote_springapi'));
+        }
+    })
+    .match('/static/ueditor/ueditor.config.js', {
+        postprocessor: function (content, file, settings) {
+            return content
+                .replace('__SERVER_URL__', fis.get('remote_server'));
+        }
+    })
+    .match('/vendor/**/*.js', {
+        url: '${baseurl}/static/$0'
+    })
+    .match('*', {
+        deploy: fis.plugin('http-push', {
+            receiver: 'http://121.40.216.238:8083/receiver.php',
+            to: 'C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/test'
+        })
+    });
