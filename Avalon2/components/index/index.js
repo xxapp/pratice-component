4010138e4ca9a2e3ff7f16a0a8b8effb1f66a7a8
mengxiangxiang <mengxiangxiang@cdavatar.com>
2016-10-09 14:18:38 +0800

    玩玩简单组件的组合

--- Avalon2/components/index/index.js
+++ Avalon2/components/index/index.js
@@ -1,25 +1,78 @@
 var avalon = require('avalon');
 
+function heredoc(fn) {
+    return fn.toString().replace(/^[^\/]+\/\*!?\s?/, '').
+            replace(/\*\/[^\/]+$/, '').trim().replace(/>\s*</g, '><')
+}
+
+var container = heredoc(function () {
+    /*
+        <div>
+            <ms-button>sdf</ms-button>
+            <div ms-controller="dashboard">
+                <ms-button>hello</ms-button>
+            </div>
+        </div>
+    */
+});
+var button = heredoc(function () {
+    /*
+        <button ms-click="@go"><slot /></button>
+    */
+});
+
 avalon.component('ms-container', {
-    template: '<div><slot name="content" /></div>',
-    defaults: {}
+    template: container,
+    defaults: {
+        onInit: function () {
+            console.log(this);
+            console.log(arguments);
+        },
+        onReady: function () {
+            console.log(this);
+            console.log(arguments);
+        },
+        onViewChange: function () {
+            console.log(this);
+            console.log(arguments);
+        },
+        onDispose: function () {
+            console.log(this);
+            console.log(arguments);
+        }
+    }
 });
 avalon.component('ms-button', {
-    template: '<button ms-click="@go">{{@text}}</button>',
+    template: button,
+    soleSlot: 'text',
     defaults: {
         text: 'Click Me',
         go: function () {
             alert('didimermer');
+        },
+        onInit: function () {
+            console.log(this);
+            console.log(arguments);
+        },
+        onReady: function () {
+            console.log(this);
+            console.log(arguments);
+        },
+        onViewChange: function () {
+            console.log(this);
+            console.log(arguments);
+        },
+        onDispose: function () {
+            console.log(this);
+            console.log(arguments);
         }
     }
 });
 
 avalon.define({
-    $id: 'root',
-    version: avalon.version
+    $id: 'root'
 });
 avalon.define({
     $id: 'dashboard'
 });
-
-avalon.scan();
+avalon.scan(document.body);
